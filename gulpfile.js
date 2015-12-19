ee3cd7899dd6a0f5bac96571f9fbc2d7e8067465
egy186 <egy186@yahoo.co.jp>
2015-12-19 23:38:03 +0900

    build without gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
-const del = require('del');
 const execSync = require('child_process').execSync;
 const gulp = require('gulp');
 const mustache = require('gulp-mustache');
-const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
 
 const icojsDoc = JSON.parse(execSync('"node_modules/.bin/jsdoc" src/ico.js -X')).filter(d => {
   return d.memberof === 'ICO' && (!d.access || d.access === 'public');
@@ -26,22 +20,6 @@ const icojsDoc = JSON.parse(execSync('"node_modules/.bin/jsdoc" src/ico.js -X'))
     });
   }
   return d;
-});
-
-gulp.task('build', () => {
-  const destPath = 'dist';
-  del.sync(destPath);
-  const b = browserify({
-    entries: './src/ico.js',
-    debug: true
-  }).exclude('canvas').exclude('buffer');
-  return b.bundle()
-    .pipe(source('ico.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(destPath));
 });
 
 gulp.task('docs', () => {
