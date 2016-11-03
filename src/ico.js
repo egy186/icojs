d8a33c27913bf063fe465bf291a7f579640ea450
egy186 <egy186@yahoo.co.jp>
2016-11-03 23:38:44 +0900

    add getImageData

--- src/ico.js
+++ src/ico.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const extractOne = require('./extract-one');
-const imageData = require('./image-data');
-
-const range = n => new Array(n).fill(0).map((_, i) => i);
+const parseICO = require('./parse-ico');
+const isICO = require('./is-ico');
 
 /**
-* @class ICO
-*/
+ * @class ICO
+ */
 
 const factory = config => {
   const previousICO = global.ICO;
@@ -25,43 +23,20 @@ const factory = config => {
      *   * `buffer` **ArrayBuffer** - Image buffer.
      */
     parse (buffer, mime) {
-      const icoDv = new DataView(buffer);
-      if (icoDv.getUint16(0, true) !== 0 || icoDv.getUint16(2, true) !== 1) {
-        return Promise.reject(new Error('buffer is not ico'));
+      try {
+        const icos = parseICO(buffer)
+          .map(ico => Image.encode(ico, mime)
+            .then(imageBuffer => ({
+              bit: ico.bit,
+              width: ico.width,
+              height: ico.height,
+              buffer: imageBuffer
+            }))
+          );
+        return Promise.all(icos);
+      } catch (err) {
+        return Promise.reject(err);
       }
-      // make single image icon
-      // let idCount = icoDv.getUint16(4, true);
-      const icos = Promise.all(range(icoDv.getUint16(4, true)).map(i => {
-        const ico = extractOne(buffer, i);
-        const image = {
-          width: ico.width,
-          height: ico.height
-        };
-        switch (ico.bit) { // eslint-disable-line default-case
-          case 1:
-            image.data = imageData.from1bit(ico);
-            break;
-          case 4:
-            image.data = imageData.from4bit(ico);
-            break;
-          case 8:
-            image.data = imageData.from8bit(ico);
-            break;
-          case 24:
-            image.data = imageData.from24bit(ico);
-            break;
-          case 32:
-            image.data = imageData.from32bit(ico);
-            break;
-        }
-        return Image.encode(image, mime).then(pngBuffer => ({
-          bit: ico.bit,
-          width: ico.width,
-          height: ico.height,
-          buffer: pngBuffer
-        }));
-      }));
-      return icos;
     },
     /**
      * Check the ArrayBuffer is valid ICO.
@@ -70,13 +45,7 @@ const factory = config => {
      * @returns {Boolean} True if arg is ICO.
      */
     isICO (buffer) {
-      if (!(buffer instanceof ArrayBuffer)) {
-        return false;
-      }
-      const icoDv = new DataView(buffer);
-      // idReserved = icoDv.getUint16(0, true)
-      // idType = icoDv.getUint16(0, true)
-      return icoDv.getUint16(0, true) === 0 && icoDv.getUint16(2, true) === 1;
+      return isICO(buffer);
     },
     /**
      * No conflict.
---
+++ src/parse-bmp.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const bitArray = require('./utils/bit-array');
+const range = require('./utils/range');
+const toDividableBy4 = require('./utils/to-dividable-by-4');
+const getImageData = require('./get-image-data');
+
+/**
+ * parse bitmap
+ * @access private
+ * @param {Number} width bitmap width
+ * @param {Number} height bitmap height
+ * @param {ArrayBuffer} arrayBuffer bitmap buffer
+ * @returns {Object} imageData imageData of bitmap
+ * @returns {Number} imageData.width image width
+ * @returns {Number} imageData.height image height
+ * @returns {Number} imageData.bit image bit depth
+ * @returns {Uint8ClampedArray} imageData.data xor image
+ */
+const parseBMP = (width, height, arrayBuffer) => {
+  const dataView = new DataView(arrayBuffer);
+
+  const headerSize = dataView.getUint32(0, true);
+  const bit = dataView.getUint16(14, true);
+  const colorsCount = dataView.getUint32(32, true);
+  const xorOffset = headerSize + (colorsCount * 4);
+  const andOffset = xorOffset + (toDividableBy4(width * bit / 8) * height);
+
+  const bitmap = {
+    and: arrayBuffer.slice(andOffset, andOffset + (toDividableBy4(width / 8) * height)),
+    bit,
+    colors: range(colorsCount)
+      .map(index => {
+        const length = 4;
+        const offset = headerSize + (index * length);
+        return bitArray.of8(arrayBuffer.slice(offset, offset + length));
+      }),
+    height,
+    width,
+    xor: arrayBuffer.slice(xorOffset, andOffset)
+  };
+  return Object.assign(getImageData(bitmap), { bit });
+};
+
+module.exports = parseBMP;
