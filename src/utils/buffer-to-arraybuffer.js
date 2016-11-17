192775377f36933644d702a4a7e211c660c1c39a
egy186 <egy186@yahoo.co.jp>
2016-11-17 21:53:38 +0900

    buffer handling

--- src/utils/buffer-to-arraybuffer.js
+++ src/utils/buffer-to-arraybuffer.js
@@ -1,12 +1,8 @@
 'use strict';
 
 const bufferToArrayBuffer = buffer => {
-  const ab = new ArrayBuffer(buffer.length);
-  const view = new Uint8Array(ab);
-  for (let i = 0; i < buffer.length; ++i) {
-    view[i] = buffer[i];
-  }
-  return ab;
+  const ab = new Uint8Array(buffer);
+  return ab.buffer;
 };
 
 module.exports = bufferToArrayBuffer;
