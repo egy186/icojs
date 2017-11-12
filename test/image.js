b212cd9bf3de23fcc673cd8fce091963cbcd7b5a
egy186 <egy186@yahoo.co.jp>
2017-11-12 11:24:53 +0900

    use bmp-js, jpeg-js, and pngjs

--- test/image.js
+++ test/image.js
@@ -4,6 +4,7 @@ const Image = require('../src/node/image');
 const bufferToArrayBuffer = require('../src/node/buffer-to-arraybuffer');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const fileType = require('file-type');
 const fs = require('fs');
 const isSame = require('./fixtures/is-same');
 const path = require('path');
@@ -13,29 +14,46 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('Image', () => {
-  describe('.decode', () => {
+  describe('.decodeSync', () => {
+    it('is expected to throw error when mime is not supported', () => {
+      const buffer = Buffer.from([]);
+      const arrayBuffer = bufferToArrayBuffer(buffer);
+      expect(() => Image.decodeSync(arrayBuffer)).to.throw(TypeError);
+    });
     it('is expected to create ImageData from PNG', () => {
       const buffer = fs.readFileSync(path.join(__dirname, './fixtures/images/1x1/1x1-1bit.png'));
       const arrayBuffer = bufferToArrayBuffer(buffer);
-      const promise = Image.decode(arrayBuffer);
-      return Promise.all([
-        expect(promise).to.eventually.have.property('data').that.is.deep.equal(new Uint8ClampedArray([0, 0, 0, 0])),
-        expect(promise).to.eventually.have.property('height').that.is.deep.equal(1),
-        expect(promise).to.eventually.have.property('width').that.is.deep.equal(1)
-      ]);
+      const imageData = Image.decodeSync(arrayBuffer);
+      expect(imageData.data).to.deep.equal(new Uint8ClampedArray([0, 0, 0, 0]));
+      expect(imageData.height).to.deep.equal(1);
+      expect(imageData.width).to.deep.equal(1);
     });
   });
-  describe('.encode', () => {
-    it('is expected to create PNG from ImageData', () => {
-      const promise = Image.encode({
+  describe('.encodeSync', () => {
+    it('is expected to create 1x1 PNG from ImageData', () => {
+      const imageArrayBuffer = Image.encodeSync({
         width: 1,
         height: 1,
         data: new Uint8ClampedArray([0, 0, 0, 0])
       });
-      return Promise.all([
-        expect(promise).to.eventually.instanceof(ArrayBuffer),
-        expect(promise.then(ab => isSame(ab, '1x1/1x1-1bit.png'))).to.become(true)
-      ]);
+      expect(imageArrayBuffer).to.be.an.instanceof(ArrayBuffer);
+      expect(fileType(imageArrayBuffer).mime).to.deep.equal('image/png');
+      return expect(isSame(imageArrayBuffer, '1x1/1x1-1bit.png')).to.become(true);
+    });
+    const mimeTypes = [
+      'image/bmp',
+      'image/jpeg',
+      'image/png'
+    ];
+    mimeTypes.forEach(mime => {
+      it(`is expected to create ${mime} from ImageData`, () => {
+        const imageArrayBuffer = Image.encodeSync({
+          width: 1,
+          height: 1,
+          data: new Uint8ClampedArray([0, 0, 0, 0])
+        }, mime);
+        expect(fileType(imageArrayBuffer).mime).to.deep.equal(mime);
+      });
     });
   });
 });
