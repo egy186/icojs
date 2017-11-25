c239a3c5f922cd5fe5fbe7f5b51dcee375878dd8
egy186 <egy186@yahoo.co.jp>
2017-11-26 00:01:18 +0900

    fix: use bmp-js, jpeg-js, and pngjs  (#58)

--- src/node/image.js
+++ src/node/image.js
@@ -1,24 +1,83 @@
 'use strict';
 
 const Buffer = require('safe-buffer').Buffer;
-const Jimp = require('jimp');
-
+const PNG = require('pngjs').PNG;
+const bmp = require('bmp-js');
 const bufferToArrayBuffer = require('./buffer-to-arraybuffer');
+const fileType = require('file-type');
+const jpeg = require('jpeg-js');
+
+const MIME_BMP = 'image/bmp';
+const MIME_JPEG = 'image/jpeg';
+const MIME_PNG = 'image/png';
+
+const decoders = {
+  [MIME_BMP]: bmp.decode,
+  [MIME_JPEG]: jpeg.decode,
+  [MIME_PNG]: PNG.sync.read
+};
+
+const encoders = {
+  [MIME_BMP]: imageData => bmp.encode(imageData).data,
+  [MIME_JPEG]: imageData => jpeg.encode(imageData).data,
+  [MIME_PNG]: PNG.sync.write
+};
 
 const Image = {
+  /**
+   * Asynchronously create imageData from image
+   * @access private
+   * @param {ArrayBuffer} arrayBuffer image buffer
+   * @returns {Promise<ImageData>} Resolves to imageData
+   */
+  decode (arrayBuffer) {
+    try {
+      const imageData = this.decodeSync(arrayBuffer);
+      return Promise.resolve(imageData);
+    } catch (err) {
+      /* istanbul ignore next */
+      return Promise.reject(err);
+    }
+  },
   /**
    * Create imageData from image
    * @access private
    * @param {ArrayBuffer} arrayBuffer image buffer
    * @returns {ImageData} imageData
    */
-  decode (arrayBuffer) {
+  decodeSync (arrayBuffer) {
     const buffer = Buffer.from(arrayBuffer);
-    return Jimp.read(buffer).then(image => ({
-      width: image.bitmap.width,
-      height: image.bitmap.height,
-      data: new Uint8ClampedArray(image.bitmap.data)
-    }));
+    const type = fileType(buffer);
+    const mime = type ? type.mime : null;
+    if (!(mime in decoders)) {
+      throw new TypeError(`${mime} is not supported`);
+    }
+    const decoder = decoders[mime];
+    const imageData = decoder(buffer);
+    return {
+      data: new Uint8ClampedArray(imageData.data),
+      height: imageData.height,
+      width: imageData.width
+    };
+  },
+  /**
+   * Asynchronously create image from imgData.data
+   * @access private
+   * @param {Object} image data
+   * @param {Number} image.width img width
+   * @param {Number} image.height img height
+   * @param {Uint8ClampedArray} image.data same as imageData.data
+   * @param {String} [mime=image/png] MIME type
+   * @returns {Promise<ArrayBuffer>} Resolves to image
+   */
+  encode (image, mime) {
+    try {
+      const imageArrayBuffer = this.encodeSync(image, mime);
+      return Promise.resolve(imageArrayBuffer);
+    } catch (err) {
+      /* istanbul ignore next */
+      return Promise.reject(err);
+    }
   },
   /**
    * Create image from imgData.data
@@ -30,25 +89,15 @@ const Image = {
    * @param {String} [mime=image/png] MIME type
    * @returns {ArrayBuffer} image
    */
-  encode (image, mime) {
-    const data = image.data;
-    const jimp = new Jimp(image.width, image.height);
-    jimp.scan(0, 0, jimp.bitmap.width, jimp.bitmap.height, function scan (x, y, idx) {
-      this.bitmap.data[idx + 0] = data[idx + 0]; // eslint-disable-line no-invalid-this
-      this.bitmap.data[idx + 1] = data[idx + 1]; // eslint-disable-line no-invalid-this
-      this.bitmap.data[idx + 2] = data[idx + 2]; // eslint-disable-line no-invalid-this
-      this.bitmap.data[idx + 3] = data[idx + 3]; // eslint-disable-line no-invalid-this
-    });
-    return new Promise((resolve, reject) => {
-      jimp.getBuffer(mime || Jimp.MIME_PNG, (err, buffer) => {
-        /* istanbul ignore if */
-        if (err) {
-          reject(err);
-        } else {
-          resolve(bufferToArrayBuffer(buffer));
-        }
-      });
-    });
+  encodeSync (image, mime) {
+    const imageData = {
+      data: Buffer.from(image.data),
+      height: image.height,
+      width: image.width
+    };
+    const encoder = mime in encoders ? encoders[mime] : encoders[MIME_PNG];
+    const imageBuffer = encoder(imageData);
+    return bufferToArrayBuffer(imageBuffer);
   }
 };
 
--- test/fixtures/is-same.js
+++ test/fixtures/is-same.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const Buffer = require('safe-buffer').Buffer;
-const Jimp = require('jimp');
+const Image = require('../../src/node/image');
+const bufferToArrayBuffer = require('../../src/node/buffer-to-arraybuffer');
+const fs = require('fs');
 const path = require('path');
+const pixelmatch = require('pixelmatch');
 
-const isSame = (arrayBuffer, fileName) => Promise.all([
-  Jimp.read(Buffer.from(arrayBuffer)),
-  Jimp.read(path.join(__dirname, 'images', fileName))
-]).then(images => {
-  const diff = Jimp.diff(images[0], images[1], 0).percent;
+const isSame = (arrayBuffer, fileName) => {
+  const img1 = Image.decodeSync(arrayBuffer);
+  const arrayBuffer2 = bufferToArrayBuffer(fs.readFileSync(path.join(__dirname, 'images', fileName)));
+  const img2 = Image.decodeSync(arrayBuffer2);
+  const diff = pixelmatch(img1.data, img2.data, null, img1.width, img1.height, {
+    threshold: 0,
+    includeAA: true
+  });
   return diff === 0;
-});
+};
 
 module.exports = isSame;
