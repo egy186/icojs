7e36a498c908545127c6dc4682c97562c3dc1044
egy186 <egy186@gmail.com>
2023-12-13 12:03:17 +0900

    fix: fix type declaration of default export (#685)

--- types/index.d.ts
+++ types/index.d.ts
@@ -12,7 +12,13 @@ declare const parseICO: (
   mime?: string
 ) => Promise<Array<ParsedImage>>;
 
-declare const ICO: { isICO, parseICO };
+declare const ICO: {
+  isICO: (source: ArrayBuffer | Buffer) => Boolean;
+  parseICO: (
+    buffer: ArrayBuffer | Buffer,
+    mime?: string
+  ) => Promise<Array<ParsedImage>>;
+};
 
 export type { ParsedImage };
 
