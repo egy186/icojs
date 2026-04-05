138b68353ecb0c2cf4c2ff508aa8f50485b1d09a
egy186 <egy186@gmail.com>
2026-04-06 08:52:08 +0900

    fix: revert changes in the isSame function

--- src/test-fixtures/is-same.js
+++ src/test-fixtures/is-same.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises';
 
 const isSame = async (arrayBuffer, fileName) => {
   const img1 = await Image.decode(arrayBuffer);
-  const arrayBuffer2 = await readFile(new URL(`./images/${fileName}`, new URL(import.meta.url)));
+  const arrayBuffer2 = await readFile(new URL(`images/${fileName}`, import.meta.url));
   const img2 = await Image.decode(arrayBuffer2);
   const diff = pixelmatch(img1.data, img2.data, null, img1.width, img1.height, {
     includeAA: true,
