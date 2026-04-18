ee7c07399aec0f911e617584f704edbdd916da59
egy186 <egy186@gmail.com>
2026-04-18 23:55:53 +0900

    feat!: remove deprecated parseICO and isICO

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -6,26 +6,13 @@ import { isIco } from '../is-ico.js';
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 const decodeIco = async (arrayBuffer: ArrayBuffer, mime = 'image/png'): Promise<Array<DecodeImage>> => await decode(arrayBuffer, mime, Image);
 
-/** @deprecated Use `isIco` instead. */
-const isICO = isIco;
-
-// eslint-disable-next-line jsdoc/require-param, jsdoc/require-returns
-/** @deprecated Use `decodeIco` instead. */
-// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-const parseICO = async (arrayBuffer: ArrayBuffer, mime = 'image/png'): Promise<Array<DecodeImage>> => await decode(arrayBuffer, mime, Image);
-
 const ICO = {
   decodeIco,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  isICO,
-  isIco,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  parseICO
+  isIco
 };
 
 export type { DecodeImage };
 
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-export { decodeIco, isICO, isIco, parseICO };
+export { decodeIco, isIco };
 
 export default ICO;
