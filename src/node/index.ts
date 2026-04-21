5116fc46b5ea8a5f61a23cc119768e38f7f083b3
egy186 <egy186@gmail.com>
2026-04-21 22:16:24 +0900

    feat!: remove deprecated parseICO and isICO (#927)
    * Use `decodeIco` and `isIco` instead.

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,7 +1,6 @@
 import type { DecodeImage } from '../decode.js';
 import Image from './image.js';
 import { decode } from '../decode.js';
-import { deprecate } from 'node:util';
 import { isIco } from '../is-ico.js';
 
 /**
@@ -14,38 +13,13 @@ import { isIco } from '../is-ico.js';
 // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
 const decodeIco = async (buffer: ArrayBuffer | Buffer, mime = 'image/png'): Promise<Array<DecodeImage>> => await decode(buffer, mime, Image);
 
-/** @deprecated Use {@link isIco} instead. */
-const isICO = deprecate(
-  isIco,
-  'isICO is deprecated. Use isIco instead.'
-);
-
-/**
- * Parse ICO and return some images.
- *
- * @param buffer - ICO file data.
- * @param mime - MIME type for output.
- * @returns Resolves to an array of {@link DecodeImage}.
- * @deprecated Use {@link decodeIco} instead.
- */
-const parseICO = deprecate(
-  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
-  async (buffer: ArrayBuffer | Buffer, mime = 'image/png'): Promise<Array<DecodeImage>> => await decode(buffer, mime, Image),
-  'parseICO is deprecated. Use decodeIco instead.'
-);
-
 const ICO = {
   decodeIco,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  isICO,
-  isIco,
-  // eslint-disable-next-line @typescript-eslint/no-deprecated
-  parseICO
+  isIco
 };
 
 export type { DecodeImage };
 
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-export { decodeIco, isICO, isIco, parseICO };
+export { decodeIco, isIco };
 
 export default ICO;
